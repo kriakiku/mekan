84d95cf991e793b98aeb251b74e7409d973be303
Jonas Otter <jonas.otter@winfinity.live>
2022-11-07 17:30:41 +0200

    feat: api

--- server/api/menu.ts
+++ server/api/menu.ts
@@ -1,6 +1,16 @@
 import { getMenu } from "../data/menu"
 
 export default defineEventHandler(async (event) => {
-    event.res.setHeader('cache-control', 'max-age=3600,stale-while-revalidate=36000,stale-if-error=36000')
-    return await getMenu()
+    try {
+        const menu = await getMenu()
+        event.res.setHeader('cache-control', 'max-age=3600,stale-while-revalidate=36000,stale-if-error=36000')
+        return menu
+    } catch (error) {
+        event.res.setHeader('cache-control', 'no-cache')
+        console.error(error)
+        return <MenuResponse>{
+            items: [],
+            sections: []
+        }
+    }
 })
