73bbd54e506d25cc840e14b93524f801786cdd04
Jonas Otter <jonas.otter@winfinity.live>
2022-11-07 16:23:56 +0200

    feat: translate

--- server/data/translate.ts
+++ server/data/translate.ts
@@ -2,7 +2,8 @@ import {TranslationServiceClient} from '@google-cloud/translate';
 import key from '../../key.json'
 
 const translationClient = new TranslationServiceClient({
-    keyFile: './key.json'
+    projectId: key.project_id,
+    credential: key
 });
 
 const projectId = 'dataset-364920';
@@ -16,7 +17,7 @@ export async function translate(
     // Construct request
     const request = {
         parent: `projects/${projectId}/locations/${location}`,
-        contents: texts,
+        contents: texts.map(item => item),
         mimeType: 'text/plain',
         sourceLanguageCode: options.from,
         targetLanguageCode: options.to,
@@ -29,10 +30,30 @@ export async function translate(
         .translations
         .map(
             item => item.translatedText
-                .replaceAll('Одноместный', 'Одиночный')
-                .replaceAll('Одномісний', 'Одиночний')
-                .replaceAll('STARTERS AND ENTERTAINMENTS', 'Snacks and starters')
-                .replaceAll('НАЧАЛО И РАЗВЛЕЧЕНИЯ', 'Снеки и стартеры')
-                .replaceAll('ПОЧАТКИ ТА РОЗВАГИ', 'Снеки та стартери')
+                .replace(/Одноместный/ig, 'Одиночный')
+                .replace(/Одномісний/ig, 'Одиночний')
+                .replace(/ENTERTAINMENTS/ig, 'Snacks')
+                .replace(/НАЧАЛО И РАЗВЛЕЧЕНИЯ/ig, 'Снеки и стартеры')
+                .replace(/ПОЧАТКИ ТА РОЗВАГИ/ig, 'Снеки та стартери')
+                .replace(/ПЛАТЬЯ/ig, 'Буррито')
+                .replace(/СУКНІ/ig, 'Бурітто')
+                .replace(/ТАБАККО/ig, 'Кальян')
+                .replace(/ТАБАК/ig, 'Кальян')
+                .replace(/ТЮТЮН/ig, 'Кальян')
+                .replace(/ВАТЕРМОН/ig, 'Арбузный')
+                .replace(/КАВАН /ig, 'Кавуновий ')
+                .replace(/ГАЗОВАНА ГАЗОВАНА/ig, 'Сода')
+                .replace(/СОДОВАНА/ig, 'ГАЗОВАНА')
+                .replace(/СТЕКЛЯННАЯ/ig, 'Стакан')
+                .replace(/ЧАЙ со льдом/ig, 'Холодный чай')
+                .replace(/ЛЕДЯНОЙ ЧАЙ/ig, 'Холодный чай')
+                .replace(/З ХОЛОДНИМ ЧАЄМ/ig, 'ХОЛОДНИЙ ЧАЙ')
+                .replace(/Холодный чай ЗАВТРАК/ig, 'Холодный чай Лимоном')
+                .replace(/СНІДАНОК ХОЛОДНИЙ ЧАЙ/ig, 'Холодний чай з лимоном')
+                .replace(/ЗАРЯДНОЕ УСТРОЙСТВО/ig, 'Энергетик "CHARGER"')
+                .replace(/ЗАРЯДНИЙ ПРИСТРІЙ/ig, 'Енергетик "CHARGER"')
+                .replace(/СЕМЬ ВВЕРХ/ig, 'SEVEN UP')
+                .replace(/СІМ ВГОРУ/ig, 'SEVEN UP')
+                .replace(/КОКА-КОЛА НОЛЬ/ig, 'COCA COLA ZERO')
         )
 }
